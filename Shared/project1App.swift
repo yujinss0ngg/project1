8be8c82cc38fce51b8e14e5c256241ac22a96149
yujinss0ngg <yujinss0ng@gmail.com>
2022-07-22 18:27:53 +0900

    Initial Commit

---
+++ Shared/project1App.swift
@@ -0,0 +1,17 @@
+//
+//  project1App.swift
+//  Shared
+//
+//  Created by 송유진 on 2022/07/22.
+//
+
+import SwiftUI
+
+@main
+struct project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
